e5d96ad81e8c55e5fed42a6fa58622b63ddfe4f8
Heriberto <heribertotijerina333@gmail.com>
2019-01-02 11:55:24 -0600

    variable de entorno mongo

--- Server/config/config.js
+++ Server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Hery123@ds247674.mlab.com:47674/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
