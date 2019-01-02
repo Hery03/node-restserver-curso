d5ec2bdd316e56c1301b897b667b4c01d9f1df1d
Heriberto <heribertotijerina333@gmail.com>
2019-01-02 11:33:03 -0600

    restserver -usuario

--- Server/server.js
+++ Server/server.js
@@ -3,38 +3,21 @@ require('./config/config')
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
+app.use(require('./rutas/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            body
-        })
-    }
-})
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
+mongoose.connect(process.env.URLDB, (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto ${process.env.PORT}`);
